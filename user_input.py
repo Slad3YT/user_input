34a3c9b5048f2c88f6381d8b7f6105bbffcc1bdf
Slad3YT <142502933+Slad3YT@users.noreply.github.com>
2024-02-25 19:41:51 +0200

    Add files via upload
    Python

---
+++ user_input.py
@@ -0,0 +1,8 @@
+
+name = input("What is your name? ")
+
+age = input("What is your age? ")
+
+location = input("Where are you located? ")
+
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
